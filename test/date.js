1db6eb8bbd3694ff0a9bd64d7c17292e5ba1b2bb
Prasanna <lakshmiprasanna@everest.engineering>
2020-02-04 17:00:08 +0530

    XD-599 Move isodate helper to lib from xmsui and xmsserver
    isodate helper was defined in both xmsui and xmsserver. Moved it here as it is a common helper. Moved it under date library.

---
+++ test/date.js
@@ -0,0 +1,54 @@
+"use strict";
+
+require("mocha");
+var assert = require("assert");
+var hbs = require("handlebars").create();
+const moment =  require("moment");
+const sinon = require("sinon");
+
+var helpers = require("..");
+helpers.date({ handlebars: hbs });
+
+describe("date", function() {
+  describe("isodate", function () {
+
+    beforeEach(function() {
+      this.clock = date => sinon.useFakeTimers(new Date(date));
+      this.clock('2020-02-02');
+    });
+
+    it("should return empty string for invalid method name", function () {
+      var fn = hbs.compile("{{isodate \"invalid\"}}");
+      assert.equal(fn(), "");
+    });
+
+    it("should return the current iso date", function () {
+      var fn = hbs.compile("{{isodate \"now\"}}");
+      assert.equal(fn(), moment().toISOString());
+    });
+
+    it("should return today's date", function () {
+      var fn = hbs.compile("{{isodate \"today\"}}");
+
+      assert.equal(fn(), moment()
+        .startOf("day")
+        .toISOString());
+    });
+
+    it("should add the days", function () {
+      var fn = hbs.compile("{{isodate \"add\" 2}}");
+
+      assert.equal(fn(),  moment()
+        .add(2, "days")
+        .toISOString());
+    });
+
+    it("should subtract the days", function () {
+      var fn = hbs.compile("{{isodate \"subtract\" 2}}");
+
+      assert.equal(fn(), moment()
+        .subtract(2, "days")
+        .toISOString());
+    });
+  });
+})
