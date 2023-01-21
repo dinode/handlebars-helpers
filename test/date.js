cd42e487df7c20f53a916e542203b918ccf44ab3
estephan <j.estephan4@gmail.com>
2023-01-21 21:20:35 +1100

    fix: Lock in moment package version.

--- test/date.js
+++ test/date.js
@@ -1,68 +1,62 @@
-"use strict";
+'use strict';
 
-require("mocha");
-var assert = require("assert");
-var hbs = require("handlebars").create();
-const moment =  require("moment");
-const sinon = require("sinon");
+require('mocha');
+var assert = require('assert');
+var hbs = require('handlebars').create();
+const moment = require('moment');
+const sinon = require('sinon');
 
-var helpers = require("..");
+var helpers = require('..');
 helpers.date({ handlebars: hbs });
 
-describe("date", function() {
-  describe("isodate", function () {
-
-    beforeEach(function() {
-      this.clock = date => sinon.useFakeTimers(new Date(date));
+describe('date', function () {
+  describe('isodate', function () {
+    beforeEach(function () {
+      this.clock = (date) => sinon.useFakeTimers(new Date(date));
       this.clock('2020-02-02');
     });
 
-    it("should return empty string for invalid method name", function () {
-      var fn = hbs.compile("{{isodate \"invalid\"}}");
-      assert.equal(fn(), "");
+    it('should return empty string for invalid method name', function () {
+      var fn = hbs.compile('{{isodate "invalid"}}');
+      assert.equal(fn(), '');
     });
 
-    it("should return the current iso date", function () {
-      var fn = hbs.compile("{{isodate \"now\"}}");
+    it('should return the current iso date', function () {
+      var fn = hbs.compile('{{isodate "now"}}');
+      console.log(moment);
       assert.equal(fn(), moment().toISOString());
     });
 
     it("should return today's date", function () {
-      var fn = hbs.compile("{{isodate \"today\"}}");
+      var fn = hbs.compile('{{isodate "today"}}');
 
-      assert.equal(fn(), moment()
-        .startOf("day")
-        .toISOString());
+      assert.equal(fn(), moment().startOf('day').toISOString());
     });
 
-    it("should add the days", function () {
-      var fn = hbs.compile("{{isodate \"add\" 2}}");
+    it('should add the days', function () {
+      var fn = hbs.compile('{{isodate "add" 2}}');
 
-      assert.equal(fn(),  moment()
-        .add(2, "days")
-        .toISOString());
+      assert.equal(fn(), moment().add(2, 'days').toISOString());
     });
 
-    it("should subtract the days", function () {
-      var fn = hbs.compile("{{isodate \"subtract\" 2}}");
+    it('should subtract the days', function () {
+      var fn = hbs.compile('{{isodate "subtract" 2}}');
 
-      assert.equal(fn(), moment()
-        .subtract(2, "days")
-        .toISOString());
+      assert.equal(fn(), moment().subtract(2, 'days').toISOString());
     });
 
-    it("should format date", function () {
-      var fn = hbs.compile("{{formatdate '2022-02-01T00:00:00.000+11:00' 'YYYY-MM-DD'}}");
+    it('should format date', function () {
+      var fn = hbs.compile(
+        "{{formatdate '2022-02-01T00:00:00.000+11:00' 'YYYY-MM-DD'}}"
+      );
 
       assert.equal(fn(), '2022-02-01');
     });
 
-    it("should format date when combined with isodate", function () {
+    it('should format date when combined with isodate', function () {
       var fn = hbs.compile("{{formatdate (isodate 'today') 'YYYY-MM-DD'}}");
 
-      assert.equal(fn(), moment()
-        .startOf("day")
-        .format('YYYY-MM-DD'));
+      assert.equal(fn(), moment().startOf('day').format('YYYY-MM-DD'));
     });
   });
-})
+});
